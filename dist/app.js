fba8d7a865acdfab791a0e297e646fc16d624757
TareqMonwer <tareqmonwer137@gmail.com>
2021-08-14 21:55:17 +0600

    :hammer: filtering projects with enums.

--- dist/app.js
+++ dist/app.js
@@ -85,7 +85,13 @@ class ProjectList {
         this.element = importedNode.firstElementChild;
         this.element.id = `${this.type}-projects`;
         projectsState.addListener((projects) => {
-            this.assignedProjects = projects;
+            const relevantProjects = projects.filter(project => {
+                if (this.type === 'active') {
+                    return project.status === ProjectStatus.Active;
+                }
+                return project.status === ProjectStatus.Finished;
+            });
+            this.assignedProjects = relevantProjects;
             this.renderProjects();
         });
         this.attach();
@@ -93,6 +99,7 @@ class ProjectList {
     }
     renderProjects() {
         const listEl = document.getElementById(`${this.type}-projects-list`);
+        listEl.innerHTML = '';
         for (const projItem of this.assignedProjects) {
             const listItem = document.createElement('li');
             listItem.textContent = projItem.title;
--- src/app.ts
+++ src/app.ts
@@ -120,15 +120,23 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
     
     projectsState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter(project => {
+        if (this.type === 'active') {
+          return project.status === ProjectStatus.Active;
+        }
+        return project.status === ProjectStatus.Finished;
+      })
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     })
+
     this.attach();
     this.renderContent()
   }
 
   renderProjects() {
     const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+    listEl.innerHTML = '';
     for (const projItem of this.assignedProjects) {
       const listItem = document.createElement('li');
       listItem.textContent = projItem.title;
